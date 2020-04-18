19f778502701c22ad2f449136a1ccd4072aff457
pedroaal <pedroaal04@gmail.com>
2020-04-18 15:33:52 -0500

    7

--- chat-client.py
+++ chat-client.py
@@ -15,46 +15,56 @@ name = str(input('Type your user name: '))
 server.send(name.encode())
 
 def send(event=None):
-  while True:
-    #msg = str(input('Me > ')) #get from terminal
-    msg = my_msg.get()
-    my_msg.set("")
-    server.send(msg.encode())
-    if msg == '[bye]':
-      server.close()
-      break
+  #msg = str(input('Me > ')) #get from terminal
+  msg = my_msg.get()
+  my_msg.set('')
+  server.send(msg.encode())
 
 def receive():
   while True:
-    message = server.recv(1024)
-    #print('\n'+message.decode()) #print on terminal
-    msg_list.insert(tkinter.END, message)
+    try:
+      message = server.recv(1024)
+      #print('\n'+message.decode()) #print on terminal
+      msg_list.insert(tkinter.END, message.decode())
+      if message.decode() == '[bye]':
+        server.close()
+        top.quit()
+        sys.exit()
+    except OSError:
+      break
 
 def on_closing(event=None):
   my_msg.set("[bye]")
   send()
 
 top = tkinter.Tk()
-top.title("Chatter")
-
+top.title("Yachay Chatroom")
 messages_frame = tkinter.Frame(top)
-my_msg = tkinter.StringVar()  # For the messages to be sent.
-my_msg.set("Type your messages here.")
-scrollbar = tkinter.Scrollbar(messages_frame)  # To navigate through past messages.
-# Following will contain the messages.
+
+scrollbar = tkinter.Scrollbar(messages_frame)
+
+#incomming messages box
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 msg_list.pack()
 messages_frame.pack()
 
+#message to send box and rturn event
+my_msg = tkinter.StringVar()
+my_msg.set("Type your messages here.")
+
+#send button
+send_button = tkinter.Button(top, text="Send", command=send)
+send_button.pack(side=tkinter.RIGHT)
+
 entry_field = tkinter.Entry(top, textvariable=my_msg)
 entry_field.bind("<Return>", send)
+entry_field.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
 entry_field.pack()
-send_button = tkinter.Button(top, text="Send", command=send)
-send_button.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
 thread_receive = threading.Thread(target = receive).start()
+
 tkinter.mainloop()
