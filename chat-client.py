45f44f85ef7dc64960e17b4c513f70f07ec9addc
pedroaal <pedroaal04@gmail.com>
2020-04-18 16:17:40 -0500

    8

--- chat-client.py
+++ chat-client.py
@@ -19,13 +19,14 @@ def send(event=None):
   msg = my_msg.get()
   my_msg.set('')
   server.send(msg.encode())
+  msg_list.insert(tkinter.END, 'Me > '+my_msg)
 
 def receive():
   while True:
     try:
       message = server.recv(1024)
       #print('\n'+message.decode()) #print on terminal
-      msg_list.insert(tkinter.END, message.decode())
+      v
       if message.decode() == '[bye]':
         server.close()
         top.quit()
@@ -47,21 +48,19 @@ scrollbar = tkinter.Scrollbar(messages_frame)
 msg_list = tkinter.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
 scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 msg_list.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-msg_list.pack()
 messages_frame.pack()
 
 #message to send box and rturn event
 my_msg = tkinter.StringVar()
 my_msg.set("Type your messages here.")
+entry_field = tkinter.Entry(top, textvariable=my_msg)
+entry_field.bind("<Return>", send)
+entry_field.pack(side=tkinter.LEFT, fill=tkinter.BOTH, expand=tkinter.TRUE)
 
 #send button
 send_button = tkinter.Button(top, text="Send", command=send)
 send_button.pack(side=tkinter.RIGHT)
 
-entry_field = tkinter.Entry(top, textvariable=my_msg)
-entry_field.bind("<Return>", send)
-entry_field.pack(side=tkinter.LEFT, fill=tkinter.BOTH)
-entry_field.pack()
 
 top.protocol("WM_DELETE_WINDOW", on_closing)
 
