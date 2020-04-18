0366673ab83fadadc98aed73622f80851ae5bdc7
pedroaal <pedroaal04@gmail.com>
2020-04-17 19:39:24 -0500

    3

--- chat-client.py
+++ chat-client.py
@@ -4,16 +4,19 @@ import threading
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.connect(('192.168.0.19', 4000))
 print("Connected to host: 192.168.0.19")
+name = str(input('Type your user name: '))
+server.send(name.encode())
 
 def send():
   while True:
-    msg = str(input('\nMe > '))
+    msg = str(input('Me > '))
     server.send(msg.encode())
 
 def receive():
   while True:
     message = server.recv(1024)
-    print('\n' + message.decode())
+    print(message.decode())
+    send()
 
 thread_send = threading.Thread(target = send)
 thread_send.start()
